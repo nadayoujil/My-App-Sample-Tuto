d98c60274f661ba8bd86b9e7ebbf1ab293e8b7b8
nadayoujil <77590232+nadayoujil@users.noreply.github.com>
2021-01-18 10:19:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Voila maintenant je suis entrain de m'exercer sur GitHub tout en ajoutant ce text sur ce fichier main.py qui va etre envoyer vers mon répertoire local !
+#Voila maintenant je suis entrain de m'exercer sur GitHub tout en ajoutant ce code sur ce fichier main.py qui va etre envoyer vers mon répertoire local !
 def pangramme(string):
     alphabet = "azertyuiopqsdfghjklmwxcvbn"
 count=0
